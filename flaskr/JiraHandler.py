90235d877e1af6c7e628be85187e333e58825cac
nrahul1992 <laalbaadshah@users.noreply.github.com>
2018-10-23 12:25:54 +0530

    Jira JQL processer added.

--- flaskr/JiraHandler.py
+++ flaskr/JiraHandler.py
@@ -1,7 +1,38 @@
-from jira import JIRA
+# Run this module once to create the JIRA issues collections. This will work as a data source for Chat application where
+# the user query will be processed against.
+# DB used- CHATAPP, Collection used- jiradatasource
+# jql- Modify the Jira project
+import ChatHandler
 
-jira = JIRA()
-jac = JIRA('https://www.myjira.nl')
-auth_jira = JIRA(auth=('Noupadarahul.Achary', 'yamahaYZFR1$'))
+from jira.client import JIRA
 
+
+def queryJira2():
+    jira_server = "https://www.myjira.nl"
+    jira_user = "Noupada.RahulAchary"
+    jira_password = "yamahaYZFR1$"
+
+    jira_server = {'server': jira_server}
+    jira = JIRA(options=jira_server, basic_auth=(jira_user, jira_password))
+    print("Entry into jira successfull")
+    jql = 'project = KWCMSLIVE AND issuetype in standardIssueTypes() AND status in (Closed, Done)'
+    block_size = 100
+    block_num = 0
+    while True:
+        start_idx = block_num * block_size
+        issues = jira.search_issues(jql, start_idx, block_size)
+        if len(issues) == 0:
+            # Retrieve issues until there are no more to come
+            break
+        block_num += 1
+        for issue in issues:
+            issueSummaryLexeme = ChatHandler.parseText(issue.fields.summary)
+            insertQuery = dict()
+            insertQuery['issueKey'] = issue.key
+            insertQuery['issueId'] = issue.id
+            insertQuery['issueSummary'] = issue.fields.summary
+            insertQuery['issueSummaryLexeme'] = issueSummaryLexeme
+            print(insertQuery)
+
+queryJira2()
 
